ca7552ab2bf6012ceddef2d04726c6719c56abc9
Philur <philur@yahoo.com>
2014-11-13 08:37:26 +0100

    Add Unit Tests with no actual coverage

---
+++ FlashCardGUITests/FlashCardGUITests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FlashCardGUITests
+{
+    [TestClass]
+    public class FlashCardGUITests
+    {
+        [TestMethod]
+        public void FlashCardGUITestsMethod1()
+        {
+        }
+    }
+}
---
+++ FlashCardGUITests/FlashCardGUIUnitTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FlashCardGUIUnitTests
+{
+    [TestClass]
+    public class FlashCardGUIUnitTests
+    {
+        [TestMethod]
+        public void FlashCardGUIUnitTestsMethod1()
+        {
+        }
+    }
+}
