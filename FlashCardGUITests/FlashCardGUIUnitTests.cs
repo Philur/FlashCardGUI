f57ac2b7fa1989f771fcef03883c14a14438760b
Philur <philur@yahoo.com>
2014-11-24 11:30:02 +0100

    Additional Unit Test

--- FlashCardGUITests/FlashCardGUIUnitTests.cs
+++ FlashCardGUITests/FlashCardGUIUnitTests.cs
@@ -164,5 +164,122 @@ namespace FlashCardGUIUnitTests
             UnitTestAssertException.Equals(System.ArgumentException("Must enter Add, Subtract, Multiply or Divide"));
         }
          */
+
+        //HACK Understand how to do this!
+        [TestMethod]
+        public void SetNumber1Multiplication()
+        {
+            FlashCardsController test = new FlashCardsController("m");
+
+            Assert.AreEqual(test.WorkOn, "M");
+
+            test.GenerateNumbers();
+
+            Assert.IsTrue(test.Number1 < 13);
+            Assert.IsTrue(test.Number1 > 0);
+
+        }
+
+        [TestMethod]
+        public void SetNumber2Multiplication()
+        {
+            FlashCardsController test = new FlashCardsController("m");
+
+            Assert.AreEqual(test.WorkOn, "M");
+
+            test.GenerateNumbers();
+
+            Assert.IsTrue(test.Number2 < 13);
+            Assert.IsTrue(test.Number2 > 0);
+
+        }
+
+        [TestMethod]
+        public void GetAndSetTries()
+        {
+            FlashCardsController test = new FlashCardsController("a");
+
+            Assert.AreEqual(test.Tries, 0);
+
+            test.Number1 = 50;
+            test.Number2 = 5;
+
+            test.CheckAnswer(55);
+
+            Assert.AreEqual(test.Tries, 1);
+        }
+
+        [TestMethod]
+        public void GetAndSetCorrect()
+        {
+            FlashCardsController test = new FlashCardsController("a");
+
+            Assert.AreEqual(test.Correct, 0);
+
+            test.Number1 = 50;
+            test.Number2 = 5;
+
+            test.CheckAnswer(55);
+
+            Assert.AreEqual(test.Correct, 1);
+        }
+        
+        [TestMethod]
+        public void GetAndSetScore()
+        {
+            FlashCardsController test = new FlashCardsController("a");
+
+            Assert.AreEqual(test.Score, 0);
+
+            test.Number1 = 50;
+            test.Number2 = 5;
+
+            test.CheckAnswer(55);
+
+            Assert.AreEqual(test.Score, 10);
+        }
+
+        [TestMethod]
+        public void BuildEquationAddition()
+        {
+            FlashCardsController test = new FlashCardsController("a");
+
+            test.Number1 = 50;
+            test.Number2 = 5;
+
+            Assert.AreEqual(test.BuildEquation(), "50+5");      
+        }
+        [TestMethod]
+        public void BuildEquationSubtraction()
+        {
+            FlashCardsController test = new FlashCardsController("s");
+
+            test.Number1 = 50;
+            test.Number2 = 5;
+
+            Assert.AreEqual(test.BuildEquation(), "50-5");
+        }
+
+        [TestMethod]
+        public void BuildEquationMultiplication()
+        {
+            FlashCardsController test = new FlashCardsController("m");
+
+            test.Number1 = 50;
+            test.Number2 = 5;
+
+            Assert.AreEqual(test.BuildEquation(), "50*5");
+        }
+        [TestMethod]
+
+        public void BuildEquationDivision()
+        {
+            FlashCardsController test = new FlashCardsController("d");
+
+            test.Number1 = 50;
+            test.Number2 = 5;
+
+            Assert.AreEqual(test.BuildEquation(), "50/5");
+        }
     }
 }
